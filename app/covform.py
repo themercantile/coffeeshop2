8b970d9102f30d32cdf96c0dd9bfbf76df54516a
themercantile <mercantile.code@gmail.com>
2020-05-05 22:21:49 +0800

    This is a commit. Exports to a table at present. I need to figure out how to manipulate input from the user.

--- app/covform.py
+++ app/covform.py
@@ -1,7 +1,7 @@
 #!python3 Coffee Input Form
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, BooleanField, SubmitField, SelectField, RadioField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email
 
@@ -9,8 +9,9 @@ class CoffeeForm(FlaskForm):
   username = StringField("Your Name", validators=[Length(min=2, max=30), DataRequired()])
   #email = EmailField('Email', [validators.Length(min=6, max=50), validators.Email()])  <-- Example from Reddit
   email = EmailField("Email", validators=[Length(min=6, max=40), Email(message=("Not a valid email address")), DataRequired()])
-  drink_type = SelectField("Name yer poison", choices=[("flatWhite", "Flat White"), ("cappucino", "Cappucino"), ("black", "Black"), ("mocha", "Mocha"), ("hotChoc", "Hot Chocolate")])
-  milk_type = SelectField("Milk", choices = [("fullCream", "Full Cream"), ("hilo", "Hi-Lo"), ("skim", "Skim"), ("almond", "Almond"), ("soy", "Soy"), ("none", "None")])
-  sugar = SelectField("Sugar", choices = [("one", "One"), ("two", "Two"), ("three", "Three"), ("none", "None")])
+  drink_type = RadioField("Drink", choices=[("5.00", "Flat White"), ("5.00", "Cappucino"), ("4.80", "Black Coffee"), ("5.20", "Mocha"), ("5.50", "Hot Chocolate")])
+  milk_type = SelectField("Milk", choices=[("None", "None"), ("Full Cream", "Full Cream"), ("Hi-Lo", "Hi-Lo"), ("Skim", "Skim"), ("Almond", "Almond"), ("Soy", "Soy")])
+  sugar = SelectField("Sugar", choices=[("None", "None"), ("One", "One"), ("Two", "Two"), ("Three", "Three")])
+  extras = SelectField("Extras", choices=[("None", "None"), ("Extra Shot", "Extra Shot"), ("Marshmallow", "Marshmallow"), ("Naked Sukes", "Naked Sukes")])
   payment = BooleanField("Paid")
   submit = SubmitField("Submit Order")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
 #!python3
 
-from flask import render_template
+from flask import render_template, redirect, url_for, session
 from app import app
 from app.covform import CoffeeForm
 
-@app.route("/")
-@app.route("/index")
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods = ["GET", "POST"])
 def index():
   coffeeForm=CoffeeForm()
+  if coffeeForm.validate_on_submit():
+    #sessions in Flask allow you to access data outside the local scope
+    session["coffeeData"] = [coffeeForm.drink_type.data, 
+                          coffeeForm.milk_type.data, 
+                          coffeeForm.sugar.data, 
+                          coffeeForm.extras.data,
+                          coffeeForm.email.data, 
+                          coffeeForm.username.data,
+                          coffeeForm.payment.data]
+    return redirect(url_for("order"))
   return render_template("index.html", form=coffeeForm)
+
+@app.route("/order", methods=["GET", "POST"])
+def order():
+  myData = session["coffeeData"]
+  print(myData)
+  return render_template("order.html", data=myData)
